19977080e75ff0c0ed4fba6106eb2b867a91f268
Ninhow <allamo.olsson@bonniernews.se>
2022-12-01 18:59:09 +0100

    Reformated the code since it did not cover part two

--- day-1/index.js
+++ day-1/index.js
@@ -1,11 +1,22 @@
 process.stdin.on('data', function (data) {
-    const elfsCalories = data.toString().split("\n\n");
-    let highestCalorieCount = 0
-    elfsCalories.forEach((elf)=>{
-        const sum = elf.split('\n').reduce((partialSum, value) => partialSum + (+value), 0)
-        if( highestCalorieCount < sum ){
-            highestCalorieCount = sum;
-        }
-    })
-    console.log(highestCalorieCount)
+    const partitionedData = data.toString().split("\n\n");
+    const sortedElfsByCalories = partitionedData.map((elfCalories) => 
+        elfCalories.split('\n')
+            .reduce((partialSum, value) => partialSum + (+value), 0)
+    ).sort((a, b) => a - b)
+    printAnswer(sortedElfsByCalories)
 });
+
+const printAnswer = (sortedElfs) => {
+    console.log(
+        `Puzzle 1: Find the Elf carrying the most Calories.
+        How many total Calories is that Elf carrying? ` 
+        + sortedElfs[sortedElfs.length - 1] + ' Calories'
+    )
+    console.log(
+        `Puzzle 2: Find the top three Elves carrying the most Calories.
+         How many Calories are those Elves carrying in total? `
+        + (sortedElfs[sortedElfs.length - 1] + sortedElfs[sortedElfs.length - 2] + sortedElfs[sortedElfs.length - 3]) + ' Calories'
+    )
+}
+
